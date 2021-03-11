f452d279d8899aeef6b66e7b4e462247695dc8aa
ed828a <ed828a@gmail.com>
2021-03-11 21:42:45 +1030

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import smtplib  # for creating smtp server, Python provides smtplib module
+from email.message import EmailMessage
+from string import Template  #built-in lib
+from pathlib import Path  # similar to os.path
+
+
+
+def send_custom_email():
+    html = Template(Path('index.html').read_text())
+    email = EmailMessage()
+    email['from'] = 'Andrei Neagoie'
+    email['to'] = 'ed828dev@gmail.com'
+    email['subject'] = 'You won 1,000,000 dollars!'
+
+    email.set_content(html.substitute({'name': "TinTin"}))
+
+    with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+        smtp.ehlo()  # hello message
+        smtp.starttls()  # encryption
+        smtp.login('ed828python@gmail.com', 'tomisadog')
+        # need to set Less secure app access of the account to ON
+        smtp.send_message(email)
+        print('all good boss!')
+
+
+if __name__ == '__main__':
+    send_custom_email()
